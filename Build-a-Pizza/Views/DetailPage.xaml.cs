3b3419a7f99719bb46bf71e6e0a61514fd0a9fc7
fhawo <fhawo@DESKTOP-FDVD96A>
2016-09-30 08:58:36 +0200

    Add project files.

---
+++ Build-a-Pizza/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using Build_a_Pizza.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace Build_a_Pizza.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
---
+++ Build-a-Pizza/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+using System;
+using Build_a_Pizza.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+
+namespace Build_a_Pizza.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+    }
+}
